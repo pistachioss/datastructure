d1033333cbcc033a1e60b0f3f4e4cd5e4950966c
1480447105@qq.com <1480447105@qq.com>
2017-11-27 00:25:17 +0800

    add to local

---
+++ datastructure/src/com/xiangyang/datastructure/firstchapter/Introduction/MathBase.java
@@ -0,0 +1,22 @@
+package com.xiangyang.datastructure.firstchapter.Introduction;
+/**
+ * @ClassName: MathBase 
+ * @Description: 数学基础知识
+ * @author 向阳
+ * @date 2017年11月22日 下午11:57:24
+ */
+public class MathBase {
+	
+	/**
+	 * @Description:在一组数组中取得第k个最大者
+	 * @param 数组
+	 * @param k 第k位
+	 * @return 第k个最大值
+	 */
+	public Integer getIndexMaxNum(Integer[] array,int k){
+		for (int i = 0; i < k ; i++) {
+			
+		}
+		return 0;
+	}
+}
